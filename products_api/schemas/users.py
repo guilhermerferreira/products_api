3fee4d5ec79d51268573c5e90b1afe0f5615eea6
Guilherme Rodrigues <guirodrigues2506@gmail.com>
2026-04-03 13:03:01 -0300

    feat(schemas): adiciona campos e validações

--- products_api/schemas/users.py
+++ products_api/schemas/users.py
@@ -1,5 +1,7 @@
-from pydantic import BaseModel, EmailStr
 from typing import Optional, List
+
+from datetime import datetime
+from pydantic import BaseModel, EmailStr, ConfigDict ,field_validator
 
 
 class UserSchema(BaseModel):
@@ -7,8 +9,40 @@ class UserSchema(BaseModel):
     email: EmailStr
     password: str
 
+    @field_validator('username') # @field_validator() - cria uma validação
+    def username_min_length(cls, v): # cls - própria classe
+        if len(v) < 3:
+            raise ValueError('Username deve ter pelo menos 3 caracteres')
+        return v
+    
+    @field_validator('password')
+    def password_min_length(cls, v):
+        if len(v) < 6:
+            raise ValueError('Password deve conter pelo menos 6 caracteres')
+        return v   
+
+
+class UserUpdateSchema(BaseModel):
+    username: Optional[str] = None
+    email: Optional[EmailStr] = None
+    password: Optional[str] = None
+
+    @field_validator('username') # @field_validator() - cria uma validação
+    def username_min_length(cls, v): # cls - própria classe
+        if len(v) < 3:
+            raise ValueError('Username deve ter pelo menos 3 caracteres')
+        return v
+    
+    @field_validator('password')
+    def password_min_length(cls, v):
+        if len(v) < 6:
+            raise ValueError('Password deve conter pelo menos 6 caracteres')
+        return v   
+
 
 class UserPublicSchema(BaseModel):
+    model_config= ConfigDict(from_attributes=True) # Faz que seja possvel construir um UserPublicSchema, atráves de outros objetos
+    
     id: int
     username: str
     email: EmailStr
@@ -18,9 +52,5 @@ class UserListPublicSchema(BaseModel):
     users: List[UserPublicSchema]
     offset: int
     limit: int
-
-
-class UserUpdateSchema(BaseModel):
-    username: Optional[str] = None
-    email: Optional[EmailStr] = None
-    password: Optional[str] = None
+    created_at: datetime
+    updated_at: datetime
