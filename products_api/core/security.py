0c06ad7a7acb5676c34132dc1a92d4ecfb0fe844
Guilherme Rodrigues <guirodrigues2506@gmail.com>
2026-04-22 17:20:54 -0300

    feat: add função access token
    - variaveis de ambientes adicionadas no arquivo .env
    - settings alterado - acrescentado função access token
    - cria um token com jwt

--- products_api/core/security.py
+++ products_api/core/security.py
@@ -1,7 +1,14 @@
+from datetime import datetime, timedelta, timezone
+from typing import Dict
+
+import jwt
 from pwdlib import PasswordHash
+
+from products_api.core.settings import Settings
 
 
 pwd_context = PasswordHash.recommended()
+settings = Settings()
 
 
 def get_password_hash(password: str) -> str:
@@ -14,3 +21,19 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
         se for igual retorna True
     '''
     return pwd_context.verify(plain_password, hashed_password) # verify() faz a comparação
+
+
+def create_access_token(data: Dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc()) + timedelta(
+        minutes=settings.JWT_EXPIRED_MINUTES
+    )
+
+    to_encode.update({'exp': expire})
+
+    jwt_encoded = jwt.encode(
+        to_encode, settings.JWT_SECRET_KEY, algorithm=settings.JWT_ALGORITHM
+    )
+
+    return jwt_encoded
+
